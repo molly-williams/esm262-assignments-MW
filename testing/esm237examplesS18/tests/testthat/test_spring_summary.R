15bff91481c6c19b3a879b874ad7eaf3b938cfb1
mollywilliams <mollylegate@gmail.com>
2021-03-16 13:22:47 -0700

    final changes

--- testing/esm237examplesS18/tests/testthat/test_spring_summary.R
+++ testing/esm237examplesS18/tests/testthat/test_spring_summary.R
@@ -17,5 +17,8 @@ test_that(
     expect_that(spring_summary(clim_data, spring_months=4)$mean_springT, equals(0.5))
     expect_that(spring_summary(clim_data, spring_months=1)$mean_springT, equals(1))
     expect_true(spring_summary(clim_data, spring_months=c(1:4))$coldest_springT > -10)
+
+    expect_that(function_name(dataset, function_parameter=x)$output, equals(y))
+
   }
 )
